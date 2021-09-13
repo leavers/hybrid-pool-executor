6ad348b5806a778690536d84e068bc4d7a02e9d8
Leavers <leavers930@gmail.com>
2021-09-13 23:27:18 +0800

    Initial commit

---
+++ hybrid_pool_executor/__init__.py
@@ -0,0 +1,9 @@
+from hybrid_pool_executor.executor import HybridPoolExecutor
+
+__version__ = '0.1.0'
+__author__ = 'Leavers'
+__contact__ = 'leavers930@gmail.com'
+
+__all__ = (
+    'HybridPoolExecutor',
+)
