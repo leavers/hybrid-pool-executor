845d32ff0f8793acb46ad6bbcba23bcd02c84eab
leavers <leavers@users.noreply.github.com>
2022-06-03 18:43:10 +0800

    Bump version 0.0.2 -> 0.0.3 (#13)

--- hybrid_pool_executor/__init__.py
+++ hybrid_pool_executor/__init__.py
@@ -2,7 +2,7 @@ from hybrid_pool_executor.base import Future
 from hybrid_pool_executor.executor import HybridPoolExecutor
 from hybrid_pool_executor.spec import spec_factory as SpecFactory
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __author__ = "Leavers"
 __contact__ = "leavers930@gmail.com"
 
